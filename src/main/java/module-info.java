aa2e0621e2302f349af6454f25362b46f746b054
Kayne <student.160774@worcesterschools.net>
2026-05-01 10:56:08 -0400

    Added validation to stuff

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,15 @@
 module com.example.cosmoform {
     requires javafx.controls;
     requires javafx.fxml;
-<<<<<<< HEAD
+
     requires java.sql;
-=======
+
     requires javafx.graphics;
->>>>>>> origin/main
+
 
 
     opens com.example.cosmoform to javafx.fxml;
     exports com.example.cosmoform;
 }
+
+
